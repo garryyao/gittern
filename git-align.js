9198534c8898556e3f5d511c792d4c520c6c5589
Garry Yao <yaojun85@gmail.com>
2014-07-06 21:46:09 +0800

    fix(align) rewrite git-align in shelljs

---
+++ git-align.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+require('shelljs/global');
+
+// discard any local changes and align with the head of remote tracking branch
+var ret = exec('git tr', {silent: 1});
+if (ret.code) {
+	echo("Remote tracking branch NOT found.");
+}
+else {
+	var tracking = ret.output.trim();
+	// Stash local changes first.
+	exec('git stash save');
+	// Discard local changes in favor of the remote.
+	exec('git reset --hard ' + tracking);
+}
--- git-tr.js
+++ git-tr.js
@@ -3,4 +3,11 @@ require('shelljs/global');
 
 // Get the remote tracking branch name of the specified branch, defaults to current branch
 var branch = process.argv[2] || exec('git br', {silent: 1}).output.trim();
-exec('git rev-parse --abbrev-ref --symbolic-full-name ' + branch + '@{u}');
+var ret = exec('git rev-parse --abbrev-ref --symbolic-full-name ' + branch + '@{u}', {silent: 1});
+if (ret.code) {
+	echo("remote tracking branch NOT found");
+	exit(1);
+}
+else {
+	echo(ret.output);
+}
