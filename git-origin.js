5182b396eeb291c3217005d9b03e0c084e2458ff
Garry Yao <yaojun85@gmail.com>
2014-07-07 15:49:17 +0800

    fix(git-origin) wrong greedy regexp matching origin name

--- git-origin.js
+++ git-origin.js
@@ -3,4 +3,4 @@ require('shelljs/global');
 // get the remote name of the specified branch, defaults to the current branch
 var branch = process.argv[2] || exec('git br', {silent: 1}).output.trim();
 var tracking = exec('git rev-parse --abbrev-ref --symbolic-full-name ' + branch + '@{u}', {silent: 1}).output;
-echo(/[^\s]+(?=\/)/.exec(tracking)[0]);
+echo(/[^\s]+?(?=\/)/.exec(tracking)[0]);
