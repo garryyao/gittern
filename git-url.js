d226de99187e335855563e1d94699791e50c2ecf
Garry Yao <yaojun85@gmail.com>
2014-07-27 21:49:49 +0800

    add to git-url the feature of creating a file line link

--- git-url.js
+++ git-url.js
@@ -1,36 +1,104 @@
 #!/usr/bin/env node
+
 require('shelljs/global');
 var util = require('util');
-/*
- Generate the Git web URL that points to the specified version.
- currently support the following GIT web sites:
-  - Github
-  - Atlassian Stash
-*/
-var ref = process.argv[2] || 'HEAD';
-var hash = exec([
-  'git rev-parse --short',
-  ref
-].join(' '), {
-  silent: 1
-}).output;
-var remotes = exec('git remote show -n origin', {
-  silent: 1
-}).output;
-var repo_url = remotes.match(/(:?Fetch URL:)(.+)/)[2].trim();
-var link, repo_name, user, project;
+var fs = require('fs');
+var path = require('path');
+var cli = require('commander');
+
+cli
+  .description('create specific type of git web URL')
+  .usage('[options] (<file_path> [line_number]) | (<ref>)')
+  .option('-c, --clipboard', 'Send the generated URL to clipboard')
+  .parse(process.argv);
+
+var arg1 = cli.args[0];
+
+// is the first argument a file?
+var fstate;
+var file;
+var branch;
+var line;
+var url;
+
+if (arg1) {
+  fstate = fs.lstatSync(arg1);
+}
+
+// remote infos
+var origin = exec('git origin', {silent: 1}).output.trim();
+var remote = exec('git remote show -n ' + origin, { silent: 1 }).output;
+var endpoint = remote.match(/(:?Fetch URL:)(.+)/)[2].trim();
+var repo, user, project;
 var match;
-if (/github\.com/.exec(repo_url)) {
-  match = repo_url.match(/([^:.\/]+?)\/([^:.\/]+?)\.git/);
-  user = match[1];
-  repo_name = match[2];
-  link = util.format('https://github.com/%s/%s/commit/%s', repo_name, user, hash);
-} else if (/stash\.englishtown\.com/.exec(repo_url)) {
-  match = repo_url.match(/([^:.\/]+?)\/([^:.\/]+?)\.git/);
+var ref;
+
+// base href
+var base = {
+  'github': 'https://github.com/',
+  'stash': 'https://stash.englishtown.com/'
+};
+
+// which type of git repository?
+var type = /github\.com/.exec(endpoint) ? 'github' : /stash\.englishtown\.com/.exec(endpoint) ? 'stash' : 'unsupported';
+
+if (type === 'github') {
+  match = endpoint.match(/([^:.\/]+?)\/([^:.\/]+?)\.git/);
+  user = match[2];
+  repo = match[1];
+}
+else if (type === 'stash') {
+  match = endpoint.match(/([^:.\/]+?)\/([^:.\/]+?)\.git/);
   project = match[1];
-  repo_name = match[2];
-  link = util.format('https://stash.englishtown.com/projects/%s/repos/%s/commits/%s', project, repo_name, hash);
-} else {
+  repo = match[2];
+  url = util.format('projects/%s/repos/%s', project, repo);
+}
+
+// create link to the file (line)
+if (fstate && (fstate.isFile() || fstate.isDirectory())) {
+  file = arg1;
+  line = cli.args[1];
+  branch = exec('git br', { silent: 1 }).output.trim();
+  if (type === 'github') {
+    url = util.format('/%s/%s', repo, user);
+    url = path.join(url, 'blob', branch, file);
+    if (line) {
+      url += '#L' + line;
+    }
+  }
+  else if (type === 'stash') {
+    url = util.format('projects/%s/repos/%s', project, repo);
+    url = path.join(url, 'browse', file + '?at=refs/heads/' + branch);
+    if (line) {
+      url += '#' + line;
+    }
+  }
+}
+// create link to the commit
+else {
+  ref = exec([ 'git rev-parse --short', arg1 || 'HEAD' ].join(' '), { silent: 1 }).output;
+
+  if (type === 'github') {
+    url = util.format('%s/%s/commit/%s', repo, user, ref);
+  }
+  else if (type === 'stash') {
+    url = util.format('projects/%s/repos/%s/commits/%s', project, repo, ref);
+  }
+}
+
+// unrecognized repository type
+if (!url) {
+  echo('sorry, unsupported git endpoint:' + endpoint);
   exit(1);
 }
-echo(link.trim());
+
+url = base[type] + url;
+// where to put the created url
+if (cli.clipboard) {
+  // TODO: cross-platform clipboard
+  exec(util.format('echo "%s" | pbcopy', url.trim()));
+}
+else {
+  echo(url);
+}
+
